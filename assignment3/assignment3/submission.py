fb3e62054d12d4cbb15cf08fd3f59becba12ac27
jjodell123 <jjodell123@gmail.com>
2021-09-30 23:55:58 -0500

    compute optimal policy and policy iteration

--- assignment3/assignment3/submission.py
+++ assignment3/assignment3/submission.py
@@ -14,7 +14,7 @@ def computeQ(mdp, V, state, action):
     val = 0
     discount = mdp.discount()
     for tup in mdp.succAndProbReward(state, action):
-        val += tup[1] * (tup[2] + discount * V[state])
+        val += tup[1] * (tup[2] + discount * V[tup[0]])
     return val
 
 ############################################################
@@ -27,7 +27,7 @@ def policyEvaluation(mdp, V, pi, epsilon=0.001):
     dictionaries.
     """
     # BEGIN_YOUR_CODE (around 7 lines of code expected)
-    raise Exception("Not implemented yet")
+
     # END_YOUR_CODE
 
 ############################################################
@@ -39,9 +39,19 @@ def computeOptimalPolicy(mdp, V):
     You might find it handy to call computeQ().  Note that |V| is a
     dictionary.
     """
-    # BEGIN_YOUR_CODE (around 4 lines of code expected)
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+    policies = {}
+    for state in mdp.states:
+        max_policy = None
+        max_value = -9999999999
+        for action in mdp.actions(state):
+            val = computeQ(mdp, V, state, action)
+            if val > max_value:
+                max_value = val
+                max_policy = action
+
+        policies[state] = max_policy
+
+    return policies
 
 ############################################################
 # Problem 4.1.4
@@ -49,9 +59,27 @@ def computeOptimalPolicy(mdp, V):
 class PolicyIteration(util.MDPAlgorithm):
     def solve(self, mdp, epsilon=0.001):
         mdp.computeStates()
-        # compute |V| and |pi|, which should both be dicts
-        # BEGIN_YOUR_CODE (around 8 lines of code expected)
-        raise Exception("Not implemented yet")
+
+        V = {}
+        for state in mdp.states:
+            V[state] = 0
+            actions = mdp.actions(state)
+
+        pi = computeOptimalPolicy(mdp, V)
+
+        while True:
+            V = policyEvaluation(mdp, V, pi, epsilon)
+            newPi = computeOptimalPolicy(mdp, V)
+
+            allSame = True
+            for state in mdp.states:
+                if newPi[state] != pi[state]:
+                    allSame = False
+                    break
+
+            if allSame:
+                break
+
         # END_YOUR_CODE
         self.pi = pi
         self.V = V
